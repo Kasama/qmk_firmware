904b67dedfa8ce663bd85ca84d5335d45951338d
Kasama <robertoaall@gmail.com>
2023-11-18 22:14:24 -0300

    add: virtual keyboard stuff

--- keyboards/virtual/rp2040/keymaps/default/keymap.c
+++ keyboards/virtual/rp2040/keymaps/default/keymap.c
@@ -13,6 +13,7 @@
 #include "keymap_introspection.h"
 #include "quantum.h"
 #include "raw_hid.h"
+#include "timer.h"
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
      */
     [0] = LAYOUT_fullsize_ansi(
-        TO(1),           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
+        MO(1),           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
 
         KC_GRV,QK_BOOT,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_CAPS, KC_A,    KC_S,   TO(1),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
+        KC_CAPS, KC_A,    KC_S,   MO(1),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
     ),
@@ -73,26 +74,17 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
     } else if (data[0] == 0x45) {
         uint8_t state = data[1];
         uint8_t row = data[2];
-        uint8_t column = data[3];
+        uint8_t col = data[3];
+        bool pressed = state > 0;
 
-        uint16_t keycode = keycode_at_keymap_location(get_highest_layer(layer_state), row, column);
-
-        keyevent_t event;
-        event.pressed = state > 0;
-        event.type = KEY_EVENT;
+        keyevent_t event = MAKE_KEYEVENT(row, col, pressed);
 
         action_exec(event);
 
-        if (state > 0) {
-            register_code16(keycode);
-        } else {
-            unregister_code16(keycode);
-        }
-
         response[0] = 0x44;
         response[1] = get_highest_layer(layer_state);
-        response[2] = (keycode >> 8) & 0xFF;
-        response[3] = keycode & 0xFF;
+        // response[2] = (keycode >> 8) & 0xFF;
+        // response[3] = keycode & 0xFF;
         response[4] = 0;
         response[5] = 0xBA;
         response[6] = 0;
